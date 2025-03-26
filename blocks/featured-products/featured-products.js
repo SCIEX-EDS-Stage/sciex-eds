f63341e875d95b95294c1e7fc32ece3ff6808a1f
sushanth-sciex <sushanth.baddam@sciex.com>
2025-03-26 13:35:48 +0530

    Featured products block changes

--- blocks/featured-products/featured-products.js
+++ blocks/featured-products/featured-products.js
@@ -2,14 +2,25 @@ export default async function decorate(block) {
   const currentUrl = window.location.href;
   const relativePath = currentUrl.split('/').slice(3).join('/');
   try {
-    const response = await fetch(`/bin/sciex/tags?pagePath=/content/sciex-eds/${relativePath}`);
+    const response = await fetch(`/bin/sciex/tags?pagePath=${relativePath}`);
+
     const data = await response.json();
+    block.textContent = '';
+    const path = window.location.pathname;
+    const resp = await fetch(`${path}.plain.html`);
+    let picture = null;
+    if (resp.ok) {
+      const html = await resp.text();
+      const imageDiv = document.createElement('imageDiv');
+      imageDiv.innerHTML = html;
+      picture = imageDiv.querySelector('picture');
+    }
 
     block.classList.add('featured-products');
-
+    const dynamicElement = document.createElement('div');
     const title = document.createElement('h2');
     title.textContent = 'Featured Products';
-    block.appendChild(title);
+    dynamicElement.appendChild(title);
 
     const listContainer = document.createElement('ul');
 
@@ -24,8 +35,11 @@ export default async function decorate(block) {
       listItem.appendChild(anchor);
       listContainer.appendChild(listItem);
     });
-
-    block.appendChild(listContainer);
+    dynamicElement.appendChild(listContainer);
+    block.appendChild(dynamicElement);
+    if (picture) {
+      block.appendChild(picture);
+    }
   } catch (error) {
     console.error('Error fetching data:', error);
   }
