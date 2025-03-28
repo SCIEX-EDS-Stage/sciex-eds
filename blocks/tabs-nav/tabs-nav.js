819502b56e186dbff0d066e1dc4cef8103b87620
Venkatalakshmi-nelluri <142808398+Venkatalakshmi-nelluri@users.noreply.github.com>
2025-03-28 14:47:03 +0530

    Tabs Nav block changes

--- blocks/tabs-nav/tabs-nav.js
+++ blocks/tabs-nav/tabs-nav.js
@@ -4,5 +4,14 @@ export default async function decorate(block) {
   // Create main container div
   const blockDiv = document.createElement('div');
   blockDiv.classList.add('tw', 'tabs-nav', 'tw-bg-white');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    while (row.firstElementChild) {
+      console.log(`tabs Nav>>>>>>${row.firstElementChild}`);
+    }
+    blockDiv.append(li);
+  });
+
+  block.textContent = '';
   block.append(blockDiv);
 }
