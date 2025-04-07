21072a3db483b7721b87953eccc9862d518e09eb
subrahmanyam.yerukon@sciex.com <subrahmanyam.y@gspann.com>
2025-04-07 10:24:23 +0530

    Title block changes

--- blocks/title/title.js
+++ blocks/title/title.js
@@ -1,8 +1,20 @@
-import {} from '../../scripts/aem.js';
+import { getMetadata } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
   // Create main container div
   const blockDiv = document.createElement('div');
-  blockDiv.classList.add('tw', 'title', 'tw-bg-white');
+  blockDiv.classList.add('hero-title');
+  const heading = block.children[0].textContent;
+  const headingDiv = document.createElement('div');
+  headingDiv.classList.add('hero-heading');
+  const pageTitle = getMetadata('og:title');
+  if (heading && heading.trim() !== '') {
+    headingDiv.append(heading);
+  } else {
+    headingDiv.append(pageTitle);
+  }
+  blockDiv.append(headingDiv);
+
+  block.textContent = '';
   block.append(blockDiv);
 }
