a9d14058f2656616cb084d73206f1a658bb068f2
Venkatalakshmi-nelluri <142808398+Venkatalakshmi-nelluri@users.noreply.github.com>
2025-04-09 14:24:44 +0530

    Tabs Nav issue analysis

--- blocks/tabs-nav/tabs-nav.js
+++ blocks/tabs-nav/tabs-nav.js
@@ -3,12 +3,6 @@ import {} from '../../scripts/aem.js';
 export default async function decorate(block) {
   const blockDiv = document.createElement('div');
   blockDiv.classList.add('tabs-nav', 'tab-buttons');
-  const tabData = document.createElement('div');
-  tabData.classList.add('tab-data');
-  [...block.children].forEach((row) => {
-    console.log('row>'+row.textContent);
-  });
   block.textContent = '';
   block.append(blockDiv);
-  block.append(tabData);
 }
