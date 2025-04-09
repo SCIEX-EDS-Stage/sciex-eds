881a53fab6837ee1672e3fe3eef0fbb973a7df5c
sushanth-sciex <sushanth.baddam@sciex.com>
2025-04-09 22:48:06 +0530

    Changes in Related resources js change

--- blocks/related-resources/related-resources.js
+++ blocks/related-resources/related-resources.js
@@ -1,12 +1,13 @@
-export default function decorate(block) { 
+import { getMetadata } from '../../scripts/aem.js';
 
-    const blockDiv = document.createElement('div');
-    blockDiv.classList.add('resource');
-    const headingDiv = document.createElement('div');
-    const pageTitle = getMetadata('og:title');
-    headingDiv.append(pageTitle);
-    blockDiv.append(headingDiv);
-  
-    block.textContent = '';
-    block.append(blockDiv);
+export default function decorate(block) {
+  const blockDiv = document.createElement('div');
+  blockDiv.classList.add('resource');
+  const headingDiv = document.createElement('div');
+  const pageTitle = getMetadata('og:title');
+  headingDiv.append(pageTitle);
+  blockDiv.append(headingDiv);
+
+  block.textContent = '';
+  block.append(blockDiv);
 }
