3ebcc22c7121ebc3ab17954dbded9b19b0a3ac8c
sushanth-sciex <sushanth.baddam@sciex.com>
2025-04-09 14:56:11 +0530

    Created New block for related resource issue testing

--- blocks/related-resource/related-resource.js
+++ blocks/related-resource/related-resource.js
@@ -1,12 +1,7 @@
 export default async function decorate(block) {
   // Create main container div
   const blockDiv = document.createElement('div');
-  blockDiv.classList.add('resource');
-  const heading = block.children[1]?.textContent?.trim() || 'Related Resources';
-  const headingDiv = document.createElement('div');
-  headingDiv.classList.add('heading');
-  headingDiv.append(heading);
-  blockDiv.append(headingDiv);
+ 
 
   block.textContent = '';
   block.append(blockDiv);
