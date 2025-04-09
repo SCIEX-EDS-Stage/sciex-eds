5a9d2e27cac3345e4eb51c022bde3bc4bf01fec7
sushanth-sciex <sushanth.baddam@sciex.com>
2025-04-09 13:49:44 +0530

    Created New block for related resource bug fix

--- blocks/customer-document/customer-document.js
+++ blocks/customer-document/customer-document.js
@@ -1,18 +1,17 @@
 function createElement(tag, options = {}) {
-    const el = document.createElement(tag);
-    Object.entries(options).forEach(([key, value]) => {
-      if (key === 'classList') el.classList.add(...value);
-      else if (key === 'text') el.textContent = value;
-      else if (key === 'html') el.innerHTML = value;
-      else if (key === 'append') el.append(...value);
-      else if (key === 'addEventListener') el.addEventListener(...value);
-      else el.setAttribute(key, value);
-    });
-    return el;
-  }
+  const el = document.createElement(tag);
+  Object.entries(options).forEach(([key, value]) => {
+    if (key === 'classList') el.classList.add(...value);
+    else if (key === 'text') el.textContent = value;
+    else if (key === 'html') el.innerHTML = value;
+    else if (key === 'append') el.append(...value);
+    else if (key === 'addEventListener') el.addEventListener(...value);
+    else el.setAttribute(key, value);
+  });
+  return el;
+}
 
-export default async function decorate(block) { 
-
-    const customerDocumentDiv = createElement('div', { classList: ['tw', 'customerdocument-search', 'tw-bg-white'] });
-    block.append(customerDocumentDiv);
+export default async function decorate(block) {
+  const customerDocumentDiv = createElement('div', { classList: ['tw', 'customerdocument-search', 'tw-bg-white'] });
+  block.append(customerDocumentDiv);
 }
